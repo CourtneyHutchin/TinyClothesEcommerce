a8e668504207560455a93f8c2f22e6cf34763187
CourtneyHutchin <courtneyhutchin47@gmail.com>
2020-01-28 11:11:49 -0800

    Implemented add clothing. Created a started display invenotry page.

--- TinyClothes/Controllers/ClothesController.cs
+++ TinyClothes/Controllers/ClothesController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using TinyClothes.Data;
+using TinyClothes.Models;
 
 namespace TinyClothes.Controllers
 {
@@ -20,13 +21,30 @@ namespace TinyClothes.Controllers
         [HttpGet]
         public IActionResult ShowAll()
         {
-            return View();
+            List<Clothing> clothes = new List<Clothing>(); // Just a placeholder
+            return View(clothes);
         }
 
         [HttpGet]
         public IActionResult Add() 
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Clothing c)
+        {
+            if(ModelState.IsValid)
+            {
+                await ClothingDb.Add(c, _context);
+
+                // TempData lasts for one redirect
+                TempData["Message"] = $"{c.Title} added successfully";
+                return RedirectToAction("ShowAll");
+            }
+
+            // Return same view with validation messages
+            return View(c);
         }
     }
 }
