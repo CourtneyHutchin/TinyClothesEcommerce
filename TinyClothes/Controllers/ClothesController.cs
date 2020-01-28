376779bfa76b256af1c84865e31747f41d86997f
CourtneyHutchin <courtneyhutchin47@gmail.com>
2020-01-28 09:29:09 -0800

    Created ClothesController

---
+++ TinyClothes/Controllers/ClothesController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TinyClothes.Data;
+
+namespace TinyClothes.Controllers
+{
+    public class ClothesController : Controller
+    {
+
+        private readonly StoreContext _context;
+
+        public ClothesController(StoreContext context)
+        {
+            _context = context;      // the "this" keyword is optional since its a different name
+        }
+
+        [HttpGet]
+        public IActionResult ShowAll()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Add() 
+        {
+            return View();
+        }
+    }
+}
